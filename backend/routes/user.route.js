ac2f451981e18ee7c1ee37a970dd2bc0f978ffc7
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-10-06 17:42:09 +0000

    auth middleware and routes updated

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,31 @@
 import express from "express"
+import { authMiddleware } from "../middleware/auth.middleware.js"
 import { signup } from "../controllers/signup.js"
 import { login } from "../controllers/login.js"
 import { addProject } from "../controllers/addProject.js"
-import { authMiddleware } from "../middleware/auth.middleware.js"
+import { allProjects } from "../controllers/allProjects.js"
+import { comment } from "../controllers/comment.js"
+import { likeProject } from "../controllers/likeProject.js"
+import { viewProject } from "../controllers/viewProject.js"
+import { updateProject } from "../controllers/updateProject.js"
+import { deleteProject } from "../controllers/deleteProject.js"
+import { yourProjects } from "../controllers/yourProjects.js"
 const router = express.Router()
 
-router.post('/signup' , signup)
-router.post('/login' , login)
-router.post('/add-project' , authMiddleware , addProject)
+router.post('/signup', signup)
+router.post('/login', login)
+
+router.get('/all-projects', authMiddleware, allProjects)
+router.get('/your-projects' , authMiddleware , yourProjects)
+
+router.post('/add-project', authMiddleware, addProject)
+
+router
+    .get('/:projectId', authMiddleware, viewProject)
+    .delete('/:projectId', authMiddleware, deleteProject)
+
+router.post('/:projectId/like', authMiddleware, likeProject)
+router.patch('/:projectId/edit', authMiddleware, updateProject)
+router.post('/:projectId/comment', authMiddleware, comment)
 
 export default router 
