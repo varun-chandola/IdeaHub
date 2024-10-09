8c9a02c571b02c32d487333c1666104f60138905
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-10-09 18:28:58 +0000

    added delete and edit button on the basis of auth

--- frontend/src/components/ViewProject.jsx
+++ frontend/src/components/ViewProject.jsx
@@ -1,38 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Navbar from './Navbar.jsx'
 import CommentAndReplies from "./CommentAndReplies.jsx"
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import Loader from './Loader.jsx'
 import ProfileIcon from './ProfileIcon.jsx'
 import CommentIcon from './CommentIcon.jsx'
 import Views from './Views.jsx'
+import { authContext } from './Auth.jsx'
 
 const ViewProject = () => {
-  const [project, setProject] = useState(null)
-  const [comment, setComment] = useState('')
+  // const [project, setProject] = useState(null)
+  const { loggedInUser, setLoggedInUser, postLikes, setPostLikes, commentLikes, setCommentLikes, comments, setComments, project, setProject, commentContent, setCommentContent } = useContext(authContext)
+
   const { projectId } = useParams()
+  const navigate = useNavigate()
 
   const fetchPost = async () => {
     try {
       const response = await axios.get(`http://localhost:5000/api/v1/project/${projectId}`, { withCredentials: true })
-      console.log(response.data)
-      setProject(response?.data)
+      console.log(response.data?.project)
+      setProject(response?.data?.project)
     } catch (error) {
+      if ((error?.response?.data?.msg).includes('unauthorized')) {
+        navigate('/login')
+        toast.error(error?.response?.data?.msg)
+      }
       console.log(error)
     }
   }
 
   useEffect(() => {
     fetchPost()
-  }, [project?.likes, project?.comments])
+  }, [])
 
   const addComment = async (e) => {
     try {
       e.preventDefault()
       const response = await axios.post(`http://localhost:5000/api/v1/${projectId}/comment`, {
-        comment
+        comment: commentContent
       }, { withCredentials: true })
       console.log(response.data)
       toast.success(response?.data?.msg)
@@ -47,9 +54,21 @@ const ViewProject = () => {
       const response = await axios.post(`http://localhost:5000/api/v1/${projectId}/like`, {}, { withCredentials: true })
       console.log(response?.data)
       toast.success(response?.data?.msg)
+      if (response?.data?.msg === "Liked") setPostLikes(prev => prev + 1)
+      else setPostLikes(prev => prev - 1)
     } catch (error) {
       console.log(error?.response?.data?.msg)
       toast.error(error?.response?.data?.msg)
+    }
+  }
+
+  const deleteProject = async (projectId) => {
+    try {
+      const response = await axios.delete(`http://localhost:5000/api/v1/${projectId}/delete`, { withCredentials: true })
+      console.log(response.data)
+      toast.success(response?.data?.msg)
+    } catch (error) {
+      console.log(error?.response?.data?.msg)
     }
   }
 
@@ -61,58 +80,73 @@ const ViewProject = () => {
         {project ?
           <>
             <div className='flex flex-col items-center'>
-              <div key={project?.project?._id} className='bg-gray-100 text-black w-[60vw] rounded-xl flex justify-between flex-col p-5 mt-10 hover:cursor-pointer'>
+              <div key={project?._id} className='bg-gray-100 text-black w-[60vw] rounded-xl flex justify-between flex-col p-5 mt-10 hover:cursor-pointer'>
                 <div className='flex w-full flex-wrap'>
-                  <h1 className='text-xl font-bold hover:underline w-full'>{project?.project?.title}</h1>
+                  <h1 className='text-xl font-bold hover:underline w-full'>{project?.title}</h1>
                   <div className='flex w-full justify-between'>
                     <div className='flex items-center justify-center'>
                       <div className=''>
                         <div className='flex gap-1'>
                           <span className='flex items-center justify-center'>
                             <ProfileIcon />
-                            <p className='hover:underline hover:text-green-700'>{project?.project?.owner?.username}</p>
+                            <p className='hover:underline hover:text-green-700'>{project?.owner?.username}</p>
                           </span>
                           <span className='flex gap-1 items-center'>
                             |
                             <CommentIcon />
-                            <span>{project?.project?.comments?.length}</span>
+                            <span>{project?.comments?.length}</span>
                             <span>Comments</span>
                           </span>
                         </div>
                         <div className='flex gap-1 items-center'>
-                          <p className=''>{Date(project?.project?.createAt)?.split(' ')[1] + "/" + Date(project?.project?.createAt)?.split(' ')[2] + "/" + Date(project?.project?.createAt)?.split(' ')[3]}</p>
+                          <p className=''>{Date(project?.createAt)?.split(' ')[1] + "/" + Date(project?.createAt)?.split(' ')[2] + "/" + Date(project?.createAt)?.split(' ')[3]}</p>
                           <span className='flex gap-1 items-center'>
                             |
                             <Views />
-                            <p>{project?.project?.views}</p>
+                            <p>{project?.views}</p>
                           </span>
                         </div>
                         <div>
-                          {project?.project?.TechStack?.map(x => (
+                          {project?.TechStack?.map(x => (
                             <button key={x} className='bg-gray-200 p-1 mr-4'>{x}</button>
                           ))}
                         </div>
                       </div>
                     </div>
                     <div className='flex flex-col items-center'>
-                      <p className='mb-2'>{project?.project?.level}</p>
+                      <p className='mb-2'>{project?.level}</p>
                       <button className='text-3xl' onClick={likeProject}>👍</button>
-                      <span className='text-2xl mx-3 font-extrabold'>{project?.project?.likes}</span>
+                      <span className='text-2xl mx-3 font-extrabold'>{postLikes}</span>
                     </div>
                   </div>
                 </div>
                 <div className=''>
                   <h1 className='font-bold mt-5'>Detail :</h1>
-                  <h2>{project?.project?.content}</h2>
+                  <h2>{project?.content}</h2>
                 </div>
               </div>
+              {/* if logged in user is owner . so show delete button and update button*/}
+              {(project?.owner?.username === loggedInUser) ?
+                <div className=''>
+                  <button className='bg-red-600 p-3 rounded-xl text-white mt-5 hover:bg-red-700 mx-2' onClick={() => deleteProject(project?._id)}>Delete Project</button>
+                  <button className='bg-green-600 p-3 rounded-xl text-white mt-5 hover:bg-green-700 mx-2' onClick={() => navigate(`/project/${project?._id}/edit`, {
+                    state: {
+                      title: project?.title,
+                      TechStack: project?.TechStack,
+                      content: project?.content
+                    }
+                  })}>Edit Project</button>
+                </div>
+                : <></>
+              }
+
               <form className='mt-10 flex flex-col items-end'>
-                <textarea type='text' placeholder='add a comment' className='p-3 rounded-xl w-[60vw] outline-none bg-gray-700 text-white h-[120px]' value={comment} onChange={e => setComment(e.target.value)} required></textarea>
+                <textarea type='text' placeholder='add a comment' className='p-3 rounded-xl w-[60vw] outline-none bg-gray-700 text-white h-[120px]' value={commentContent} onChange={e => setCommentContent(e.target.value)} required></textarea>
                 <button type="submit" className="bg-green-600 text-white p-3 rounded-xl w-1/6 hover:bg-green-700" onClick={addComment} >Comment</button>
               </form>
             </div>
             <div className='flex flex-col items-center m-auto'>
-              <CommentAndReplies projectComment={project?.project?.comments} />
+              <CommentAndReplies projectComment={project?.comments} />
             </div>
           </> : <Loader />
         }
