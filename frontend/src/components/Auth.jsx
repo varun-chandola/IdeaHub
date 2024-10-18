d396dc855443ccfc9505c6b532edbd09d034682f
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-10-18 16:39:25 +0000

    added backend deployed url to auth

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -1,39 +1,39 @@
-import axios from 'axios';
-import React, { createContext, useEffect, useState } from 'react';
-import { jwtDecode } from "jwt-decode";
-
-export const authContext = createContext();
-
+import axios from 'axios'
+import React, { createContext, useEffect, useState } from 'react'
+import { jwtDecode } from "jwt-decode"
+export const authContext = createContext()
 const Auth = ({ children }) => {
-    const [loggedInUser, setLoggedInUser] = useState('');
-    const [postLikes, setPostLikes] = useState(0);
-    const [comments, setComments] = useState([]);
-    const [project, setProject] = useState(null);
-    const [userLikedPost, setUserLikedPost] = useState(false);
-    const [allProjects, setAllProjects] = useState([]);
-    const [commentLikes, setCommentLikes] = useState(0);
+    const [loggedInUser, setLoggedInUser] = useState('')
+    const [postLikes, setPostLikes] = useState(0)
+    const [comments, setComments] = useState([])
+    const [project, setProject] = useState(null)
+    const [userLikedPost, setUserLikedPost] = useState(false)
+    const [allProjects, setAllProjects] = useState([])
+    const [commentLikes, setCommentLikes] = useState(0)
+
+
 
     const yourProjects = async () => {
         try {
-            const response = await axios.get(`http://localhost:5000/api/v1/your-projects`, { withCredentials: true });
-            console.log('Your Projects\n', response);
-            setAllProjects(response.data?.yourProjects);
+            const response = await axios.get(`https://ideahub-backend.onrender.com/api/v1/your-projects`, { withCredentials: true })
+            console.log('Your Projects\n', response)
+            setAllProjects(response.data?.yourProjects)
         } catch (error) {
-            console.log(error.response?.data?.msg);
-            toast.error(error.response?.data?.msg);
+            console.log(error.response?.data?.msg)
+            toast.error(error.response?.data?.msg)
         }
-    };
-
+    }
     const YourLikedProjects = async () => {
         try {
-            const response = await axios.get(`http://localhost:5000/api/v1/your-liked-projects`, { withCredentials: true });
-            console.log(response?.data);
-            setAllProjects(response.data?.yourLikedProjects);
+            const response = await axios.get(`https://ideahub-backend.onrender.com/api/v1/your-liked-projects`, { withCredentials: true })
+            console.log(response?.data)
+            setAllProjects(response.data?.yourLikedProjects)
         } catch (error) {
-            console.log(error?.response?.data?.msg);
-            toast.error(error?.response?.data?.msg);
+            console.log(error?.response?.data?.msg)
+            toast.error(error?.response?.data?.msg)
         }
-    };
+    }
+
 
     const token = document?.cookie?.split("=")[1];
 
